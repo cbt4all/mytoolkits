0b21f99eab598c4954a6c72b62f1a0212e3202f5
Alireza <alireza@cbt4all.com>
2020-09-02 15:39:14 +1200

    add examples

--- doc.go
+++ doc.go
@@ -1,26 +1,4 @@
 // The package mytoolkits provides a bunch of functions that I ususally use in my programming
-// Some functions like
-// Create a code block
-//
-//  	func ExampleRemoveDuplicatesFromSliceString(sliceStr []string) []string {
-//
-//  		m := make(map[string]bool)
-//
-//  		for _, item := range sliceStr {
-//  			if _, DoesExist := m[item]; !DoesExist {
-//  				m[item] = true
-//  			}
-//  		}
-//
-//  		var result []string
-//
-//  		for item := range m {
-//  			result = append(result, item)
-//  		}
-//  		return result
 //
 //
-// This package provides a bunch of functions that I ususally use in my programming
-// This package provides a bunch of functions that I ususally use in my programming
-// This package provides a bunch of functions that I ususally use in my programming
 package mytoolkits
