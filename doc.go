9e7a2a218f14cfe436beb7b5eb274e593072e8c3
Alireza <alireza@cbt4all.com>
2020-09-02 14:43:52 +1200

    .

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
-// This package provides a bunch of functions that I ususally use in my programming
-
+// The package mytoolkits provides a bunch of functions that I ususally use in my programming
+// Some functions like
 // Create a code block
 //
 //  	func ExampleRemoveDuplicatesFromSliceString(sliceStr []string) []string {
