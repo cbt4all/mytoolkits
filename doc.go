231906552a53ec878fae68a0c7a44cc9a5bd3753
Alireza <alireza@cbt4all.com>
2020-09-02 14:39:32 +1200

    .

--- doc.go
+++ doc.go
@@ -2,7 +2,23 @@
 
 // Create a code block
 //
-// var a int
+//  	func ExampleRemoveDuplicatesFromSliceString(sliceStr []string) []string {
+//
+//  		m := make(map[string]bool)
+//
+//  		for _, item := range sliceStr {
+//  			if _, DoesExist := m[item]; !DoesExist {
+//  				m[item] = true
+//  			}
+//  		}
+//
+//  		var result []string
+//
+//  		for item := range m {
+//  			result = append(result, item)
+//  		}
+//  		return result
+//
 //
 // This package provides a bunch of functions that I ususally use in my programming
 // This package provides a bunch of functions that I ususally use in my programming
